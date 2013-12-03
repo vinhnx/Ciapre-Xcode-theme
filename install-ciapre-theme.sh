d78d72d1e0f30fc3edc6708c11b3f7ca5b8751d3
Vinh Nguyen <vinhnguyen2308@gmail.com>
2013-12-03 09:52:49 +0700

    update color, add install script

---
+++ install-ciapre-theme.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+destination="$HOME/Library/Developer/Xcode/UserData/FontAndColorThemes"
+
+if [[ ! -d $destination ]]; then
+    mkdir -p ${destination}
+fi
+
+find . -name "*.dvtcolortheme" -exec cp {} ${destination} \;
+
+echo "Done. Themese copied to: ${destination}"
